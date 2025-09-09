2cfd948781793d021d46cb990f4ad4531f1078a1
hivergX36 <vic.herve44@gmail.com>
2025-09-10 00:26:27 +0200

    Update_10_09_2025

--- fold_fulkerson.hpp
+++ fold_fulkerson.hpp
@@ -1,123 +1,144 @@
-#include "path.hpp"
 #include <vector>
+#include <iostream>
+#include <string>
 #include <fstream>
+#include <cstdlib>
+#include <math.h>
 #include <algorithm>
-#include <queue> 
-#include "Adjacency_list.hpp"
 
-class fold_fulkerson
+struct FoldFulkerson
 {
 
-    private:
-    int V; // Number of vertics
-    Adgency_list* adj_list; // Adjacency list representation of the graph
-    Path *result_path; // Resulting path after running Prim's algorithm
-    bool is_negative_cycle = false; // Flag to indicate if a negative cycle exists
-    bool *visited;
+    int Nbsommet;
+    float **matrix;
+    bool *Marque;
 
-    public:
-
-    fold_fulkerson(int v) : V(v) {
-        adj_list = new Adgency_list(V);
-        result_path = new Path(V);
+    FoldFulkerson()
+    {
+        Nbsommet = 0;
+        matrix = nullptr;
+        Marque = nullptr;
     }
 
-    ~fold_fulkerson() {
-        delete adj_list;
-        delete result_path;
-    }
-
-    void add_edge_components(int u, int v, int weight) {
-        adj_list->add_edge(u, v, weight);
-    }
-
-    void parse_data(std::string filename){
-     std::ifstream file(filename);
-        if (!file) {
+    void parse_file(std::string filename)
+    {
+        std::ifstream file(filename);
+        if (!file)
+        {
             std::cerr << "Error opening file: " << filename << std::endl;
             return;
-        }else{
-            int V,u, v, weight;
-            file >> V;
-            adj_list = new Adgency_list(V);
-            result_path = new Path(V);
-            visited = new bool[V];
-            for (int i = 0; i < V; ++i) {
-                visited[i] = false; // Initialize visited array
+        }
+        else
+        {
+            int u, v = 0;
+            float w = 0.0;
+            file >> Nbsommet;
+            matrix = new float *[Nbsommet];
+            for (int i = 0; i < Nbsommet; ++i)
+            {
+                matrix[i] = new float[Nbsommet];
             }
-            while (file >> u >> v >> weight) {
-                add_edge_components(u, v, weight);
+            Marque = new bool[Nbsommet];
+            for (int i = 0; i < Nbsommet; ++i)
+            {
+                Marque[i] = false;
             }
-            std::cout << "Data parsed successfully from " << filename << std::endl;
+            while (file >> u >> v >> w)
+            {
+                matrix[u][v] = w;
+            }
+            std::cout << "Matrix loaded from file:" << std::endl;
             file.close();
         }
-    };
+    }
 
-    void display_adjacency_list() {
-        adj_list->display_adjacency_list();
+    void display_graph()
+    {
+
+        for (int i = 0; i < Nbsommet; i++)
+        {
+            for (int j = 0; j < Nbsommet; j++)
+            {
+
+                std::cout << matrix[i][j] << " ";
+            }
+
+            std::cout << std::endl;
+        }
     }
 
 
-    void negative_cycle_detection() {
-        for (int i = 0; i < V; ++i) {
-            for (int j = 0; j < V; ++j) {
-                int current_vertex = j; 
-                visited[current_vertex] = true; // Mark the current vertex as visited
-                // Process the current vertex and its edges
-                for (const auto& succ : adj_list->list[current_vertex]) {
-                    int successor = succ.successor;
-                    int weight = succ.weight;
+    void Dfs(bool &visited, int &parent, int node, int sink){
+        visited[node] = True
+        if node == sink:
+            return True
+        for v in range(self.NbVertices):
+            w = self.adjacency_Matrix[node][v]
+            if visited[v] == False and w > 0:
+                parent[v] = s
+                if self.Dfs(visited, parent, v, sink):
+                    return True
+        return False
+    }
+    
 
-                    // Check if this edge can be added to the result path
-                    if (result_path->cost[successor] > weight + result_path->cost[current_vertex]) {
-                        if (visited[successor] == false){
-                            visited[successor] = true; // Mark the successor as visited
-                        } else {
-                            is_negative_cycle = true;
-                            std::cout << "Negative cycle detected involving vertex: " << successor << std::endl;
-                        }
-                 
+    void Fold_Fulkerson()
+    {
+        int pred[Nbsommet];
+        bool visited[Nbsommet];
+        int succ;
+        int pds_succ;
+        int dist = 0;
+        int Nodes[Nbsommet];
+        int chemin[Nbsommet];
+        int distance[Nbsommet];
+        int lenght;
+
+        file.push(0);
+
+        for (int i = 0; i < Nbsommet; i++)
+        {
+            Nodes[i] = i;
+            distance[i] = 100000;
+            chemin[i] = 0;
+        }
+
+        distance[0] = 0;
+        file.push(0);
+        lenght = file.size();
+
+        while (lenght > 0)
+        {
+            prec = file.front();
+            std::cout << "Le predecesseur est: " << prec << " " << std::endl;
+            file.pop();
+            for (int i = 0; i < Nbsommet; i++)
+            {
+                if (Matrice[prec][i] > 0)
+                {
+                    succ = i;
+                    pds_succ = Matrice[prec][i];
+                    std::cout << "le successeur est: " << succ << std::endl;
+                    std::cout << "le poids du sucesseur est: " << pds_succ << std::endl;
+                    dist = distance[prec] + succ;
+                    std::cout << "la distance du calcul est: " << dist << std::endl;
+                    if (dist < distance[succ])
+                    {
+
+                        distance[succ] = dist;
+                        chemin[succ] = prec;
+                        file.push(i);
                     }
                 }
-            }
-        }
-    }
+                lenght = file.size();
+                std::cout << "La longueur de la pile est: " << lenght << std::endl;
+            };
+            for (int i = 0; i < Nbsommet; i++)
+            {
 
-    void display_negative_cycle() {
-        if (is_negative_cycle) {
-            std::cout << "Negative cycle exists in the graph." << std::endl;
-        } else {
-            std::cout << "No negative cycle detected in the graph." << std::endl;
-        }
-        for (int i = 0; i < V; ++i) {
-            std::cout << visited[i] << " ";
-         
-        }
-    }
-
-
-
-    void run_BellmanFord() {
-      for (int i = 0; i < V; ++i) {
-        for (int j = 0; j < V; ++j) {
-            int current_vertex = j; 
-            // Process the current vertex and its edges
-            for (const auto& succ : adj_list->list[current_vertex]) {
-                int successor = succ.successor;
-                int weight = succ.weight;
-
-                // Check if this edge can be added to the result path
-                if (result_path->cost[successor] > weight + result_path->cost[current_vertex]) {
-                    result_path->cost[successor] = weight + result_path->cost[current_vertex];
-                    result_path->path[successor] = current_vertex;
-                }
-            }
-        }
-        // Implementation of Prim's algorithm
-    }
-}
-
-    void display_result_path() {
-        result_path->display_path();
-    }
+                std::cout << chemin[i] << " ";
+            };
+            std::cout << std::endl;
+        };
+    };
 };
